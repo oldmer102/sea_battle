9a2768f2d4ce5041cb0dccbd564f073c11c6e8ea
oldmer102 <56794465+oldmer102@users.noreply.github.com>
2022-09-05 12:04:34 +0300

    start_game

--- api.py
+++ api.py
@@ -4,12 +4,16 @@ X = 6
 Y = 6
 #словарь расстановки
 my_slov = {}
+my_slov_enemy = {'А1,Б1,В1': [[0, 0], [1, 0], [2, 0]], 'Е1,Е2': [[5, 0], [5, 1]], 'Е4,Е5': [[5, 3], [5, 4]], 'А3': [[0, 2]], 'Б5': [[1, 4]], 'Г3': [[3, 2]]}
 #словарь содержит координаты взрыва корабля
 slov_deat = {}
+slov_deat_enemy = {}
 #промежуточный список
 deat_position = []
+deat_position_enemy = []
 # проверка на занятость позиции
 busy_slot = []
+busy_slot__enemy = []
 masiv_a = [["-"] * X for i in range(Y)]
 
 
@@ -41,8 +45,13 @@ class Ship(object):
         self.coordinate = coordinate
         my_slov.update({name: coordinate})
 
+    def slovar_enemy(self, name, coordinate):
+        self.name = name
+        self.coordinate = coordinate
+        my_slov_enemy.update({name: coordinate})
+
 #Расстановка 3 коралей
-    def ship_3(self, posiv, masiv):
+    def ship_3(self, posiv, masiv, player):
         pos_b = 0
         pos_ch = 1
         pos_mas = 0
@@ -60,28 +69,49 @@ class Ship(object):
             pos_mas += 1
         if not rules.placement_check(position=mass):
             return False
+        if player == 1:
+            Ship.slovar(self, name, mass)
+            jep = rules.placement_rules_x(position=my_slov, name=name)
+            for i in jep:
+                deat_position.append(i)
+            slov_deat.update({name: jep})
+            for i in busy_slot:
+                for v in mass:
+                    if i == v:
+                        return False
+            for i in mass:
+                busy_slot.append(i)
+            for i in deat_position:
+                for v in mass:
+                    if i == v:
+                        return False
+            for k, n in mass:
+                masiv[n][k] = "■"
+            return masiv
+        if player == 2:
+            Ship.slovar_enemy(self, name, mass)
+            jep = rules.placement_rules_x(position=my_slov_enemy, name=name)
+            for i in jep:
+                deat_position_enemy.append(i)
+            slov_deat_enemy.update({name: jep})
+            for i in busy_slot__enemy:
+                for v in mass:
+                    if i == v:
+                        return False
+            for i in mass:
+                busy_slot__enemy.append(i)
+            for i in deat_position_enemy:
+                for v in mass:
+                    if i == v:
+                        return False
+            for k, n in mass:
+                masiv[n][k] = "-"
+            return masiv
 
-        Ship.slovar(self, name, mass)
-        jep = rules.placement_rules_x(position=my_slov, name=name)
-        for i in jep:
-            deat_position.append(i)
-        slov_deat.update({name: jep})
-        for i in busy_slot:
-            for v in mass:
-                if i == v:
-                    return False
-        for i in mass:
-            busy_slot.append(i)
-        for i in deat_position:
-            for v in mass:
-                if i == v:
-                    return False
-        for k, n in mass:
-            masiv[n][k] = "■"
-        return masiv
+
 
     # Расстановка 2 коралей
-    def ship_2(self, posiv, masiv):
+    def ship_2(self, posiv, masiv, player):
         pos_b = 0
         pos_ch = 1
         pos_mas = 0
@@ -100,27 +130,47 @@ class Ship(object):
 
         if not rules.placement_check(position=mass):
             return False
-        Ship.slovar(self, name, mass)
-        jep = rules.placement_rules_x(position=my_slov, name=name)
-        for i in jep:
-            deat_position.append(i)
-        slov_deat.update({name: jep})
-        for i in busy_slot:
-            for v in mass:
-                if i == v:
-                    return False
-        for i in mass:
-            busy_slot.append(i)
-        for i in deat_position:
-            for v in mass:
-                if i == v:
-                    return False
-        for k, n in mass:
-            masiv[n][k] = "■"
-        return masiv
+        if player == 1:
+            Ship.slovar(self, name, mass)
+            jep = rules.placement_rules_x(position=my_slov, name=name)
+            for i in jep:
+                deat_position.append(i)
+            slov_deat.update({name: jep})
+            for i in busy_slot:
+                for v in mass:
+                    if i == v:
+                        return False
+            for i in mass:
+                busy_slot.append(i)
+            for i in deat_position:
+                for v in mass:
+                    if i == v:
+                        return False
+            for k, n in mass:
+                masiv[n][k] = "■"
+            return masiv
+        if player == 2:
+            Ship.slovar_enemy(self, name, mass)
+            jep = rules.placement_rules_x(position=my_slov_enemy, name=name)
+            for i in jep:
+                deat_position_enemy.append(i)
+            slov_deat_enemy.update({name: jep})
+            for i in busy_slot__enemy:
+                for v in mass:
+                    if i == v:
+                        return False
+            for i in mass:
+                busy_slot__enemy.append(i)
+            for i in deat_position_enemy:
+                for v in mass:
+                    if i == v:
+                        return False
+            for k, n in mass:
+                masiv[n][k] = "-"
+            return masiv
 
     # Расстановка 1 коралей
-    def ship_1(self, posiv, masiv):
+    def ship_1(self, posiv, masiv, player):
         mass = [["", ""]]
         name = posiv
         rules = PlacementRules()
@@ -131,23 +181,44 @@ class Ship(object):
                 mass[0][0] = ord(b.lower()) - 1072
             else:
                 mass[0][1] = int(b) - 1
-        Ship.slovar(self, name, mass)
-        jep = rules.placement_rules_x(position=my_slov, name=name)
-        for i in jep:
-            deat_position.append(i)
-        slov_deat.update({name: jep})
-        for i in busy_slot:
-            for v in mass:
-                if i == v:
-                    return False
-        for i in mass:
-            busy_slot.append(i)
-        for i in deat_position:
-            for v in mass:
-                if i == v:
-                    return False
-        masiv[mass[0][1]][mass[0][0]] = "■"
-        return masiv
+        if player == 1:
+            Ship.slovar(self, name, mass)
+            jep = rules.placement_rules_x(position=my_slov, name=name)
+            for i in jep:
+                deat_position.append(i)
+            slov_deat.update({name: jep})
+            for i in busy_slot:
+                for v in mass:
+                    if i == v:
+                        return False
+            for i in mass:
+                busy_slot.append(i)
+            for i in deat_position:
+                for v in mass:
+                    if i == v:
+                        return False
+            for k, n in mass:
+                masiv[n][k] = "■"
+            return masiv
+        if player == 2:
+            Ship.slovar_enemy(self, name, mass)
+            jep = rules.placement_rules_x(position=my_slov_enemy, name=name)
+            for i in jep:
+                deat_position_enemy.append(i)
+            slov_deat_enemy.update({name: jep})
+            for i in busy_slot__enemy:
+                for v in mass:
+                    if i == v:
+                        return False
+            for i in mass:
+                busy_slot__enemy.append(i)
+            for i in deat_position_enemy:
+                for v in mass:
+                    if i == v:
+                        return False
+            for k, n in mass:
+                masiv[n][k] = "-"
+            return masiv
 
 
 class PlacementRules():
@@ -224,9 +295,12 @@ class PlacementRules():
         return zapret
 
 
+
+
+
 class StartGame():
     #запись выстрела
-    def shot(self, shot_position, array):
+    def shot_you(self, shot_position, array):
         mass = [["", ""]]
         if len(shot_position.split(",")) != 1:
             return False
@@ -236,36 +310,38 @@ class StartGame():
             else:
                 mass[0][1] = int(b) - 1
         array[mass[0][1]][mass[0][0]] = 'X'
-        return array
-# Попадание по коралю
-    def shot_ship(self,shot_position):
-        mass = [["", ""]]
-        if len(shot_position.split(",")) != 1:
-            return False
-        for b in shot_position:
-            if b.isdigit() == False:
-                mass[0][0] = ord(b.lower()) - 1072
-            else:
-                mass[0][1] = int(b) - 1
-
-        for k, v in my_slov.items():
+        for k, v in my_slov_enemy.items():
             for i in v:
                 for b in mass:
                     if i == b:
-                        my_slov.remove(i)
+                        my_slov_enemy[k].remove(i)
+        for k, v in my_slov_enemy.items():
+            if len(v) == 0:
+
+                del my_slov_enemy[k]
+                deat = slov_deat_enemy[k]
+                for i, m in deat:
+                    if i >= 0 and m >= 0:
+                        array[m][i] = 'X'
+                del slov_deat_enemy[k]
+                return array
+
+        return array
 
 #Закраска полей вокруг корабля
-    def deat_ship(self, array):
-        for k, v in my_slov.items():
-            if len(v) == 0:
-                del my_slov[k]
-                deat = slov_deat[k]
-                for i, m in deat:
-                    array[i][m] = 'X'
-                del slov_deat[k]
-                return deat
-            else:
-                return False
+    # def deat_ship(self, array):
+    #     for k, v in my_slov.items():
+    #         if len(v) == 0:
+    #
+    #             del my_slov[k]
+    #             deat = slov_deat[k]
+    #             for i, m in deat:
+    #                 if i >= 0 and m >= 0:
+    #                     array[m][i] = 'X'
+    #             del slov_deat[k]
+    #             return array
+    #         else:
+    #             return False
 
 
 
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from api import NewGame
 from api import Ship
+from api import StartGame
 from field import start_field
+from api import my_slov
 X = 6
 Y = 6
 ship = Ship()
+game = StartGame()
 masiv_a = [["-"] * X for i in range(Y)]
 masiv_e = [["-"] * X for g in range(Y)]
 amount = int(input("Сколько будет игроков, 1 или 2? "))
@@ -11,27 +14,69 @@ if amount == 1:
     name_player_1 = input('Как вас называть? ')
     new_game = NewGame(amount_player=amount, name_1=name_player_1, name_2=None)
     new_game.game_map_player_1()
+tru = False
+k = 1
+while tru == False:
+    ship_3 = input('Начнём с расстаноки, назовите координаты кораля состоящего из 3 палуб, через запятую (А1,Б1,В1)')
+    mapa = ship.ship_3(posiv=ship_3, masiv=masiv_a, player=1)
+    if mapa == False:
+        tru = False
+    else:
+        tru = True
+        start_field(masiv_you=mapa, masiv_enemy=masiv_e)
+
+tru = False
+while tru == False:
+    ship_2 = input('Назовите координаты кораля состоящего из 2 палуб, через запятую (А1,Б1)')
+    mapa = ship.ship_2(posiv=ship_2, masiv=masiv_a, player=1)
+    if mapa == False:
+        tru = False
+    else:
+        tru = True
+        start_field(masiv_you=mapa, masiv_enemy=masiv_e)
+
+tru = False
+while tru == False:
+    ship_2_1 = input('Назовите координаты кораля состоящего из 2 палуб, через запятую (А1,Б1)')
+    mapa = ship.ship_2(posiv=ship_2_1, masiv=mapa, player=1)
+    if mapa == False:
+        tru = False
+    else:
+        tru = True
+        start_field(masiv_you=mapa, masiv_enemy=masiv_e)
+
+tru = False
+while tru == False:
+    ship_1_1 = input('Назовите координаты кораля состоящего из 1 палубы, через запятую (А1)')
+    mapa = ship.ship_1(posiv=ship_1_1, masiv=mapa, player=1)
+    if mapa == False:
+        tru = False
+    else:
+        tru = True
+        start_field(masiv_you=mapa, masiv_enemy=masiv_e)
+
+tru = False
+
+while tru == False:
+    ship_1_2 = input('Назовите координаты кораля состоящего из 1 палубы, через запятую (А1)')
+    mapa = ship.ship_1(posiv=ship_1_2, masiv=mapa, player=1)
+    if mapa == False:
+        tru = False
+    else:
+        tru = True
+        start_field(masiv_you=mapa, masiv_enemy=masiv_e)
+
+tru = False
+while tru == False:
+    ship_1_3 = input('Назовите координаты кораля состоящего из 1 палубы, через запятую (А1)')
+    mapa = ship.ship_1(posiv=ship_1_3, masiv=mapa, player=1)
+    if mapa == False:
+        tru = False
+    else:
+        tru = True
+        start_field(masiv_you=mapa, masiv_enemy=masiv_e)
 
 
-
-ship_3 = input('Начнём с расстаноки, назовите координаты кораля состоящего из 3 палуб, через запятую (А1,Б1,В1)')
-mapa = ship.ship_3(posiv=ship_3, masiv=masiv_a)
-start_field(masiv_you=mapa, masiv_enemy=masiv_e)
-ship_2 = input('Назовите координаты кораля состоящего из 2 палуб, через запятую (А1,Б1)')
-mapa = ship.ship_2(posiv=ship_2, masiv=masiv_a)
-start_field(masiv_you=mapa, masiv_enemy=masiv_e)
-ship_2_1 = input('Назовите координаты кораля состоящего из 2 палуб, через запятую (А1,Б1)')
-mapa = ship.ship_2(posiv=ship_2_1, masiv=mapa)
-start_field(masiv_you=mapa, masiv_enemy=masiv_e)
-ship_1_1 = input('Назовите координаты кораля состоящего из 1 палубы, через запятую (А1)')
-mapa = ship.ship_1(posiv=ship_1_1, masiv=mapa)
-start_field(masiv_you=mapa, masiv_enemy=masiv_e)
-ship_1_2 = input('Назовите координаты кораля состоящего из 1 палубы, через запятую (А1)')
-mapa = ship.ship_1(posiv=ship_1_2, masiv=mapa)
-start_field(masiv_you=mapa, masiv_enemy=masiv_e)
-ship_1_2 = input('Назовите координаты кораля состоящего из 1 палубы, через запятую (А1)')
-mapa = ship.ship_1(posiv=ship_1_2, masiv=mapa)
-start_field(masiv_you=mapa, masiv_enemy=masiv_e)
 
 
 
